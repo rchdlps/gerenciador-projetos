bf1ddea129e0b16876ef52e73aecf0949da9924d
rchdlps <rchdlps@gmail.com>
2026-02-15 19:56:10 -0400

    chore: Add localhost:8080 and 127.0.0.1:8080 to trusted origins.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -102,6 +102,8 @@ export const auth = betterAuth({
         }
     },
     trustedOrigins: [
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
         "http://localhost:4321",
         "http://127.0.0.1:4321",
         ...(baseURL !== "http://127.0.0.1:4321" ? [baseURL] : []),
